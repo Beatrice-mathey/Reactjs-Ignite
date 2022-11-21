cc513ff41283f4336c695f493a41e729dec43110
Beatrice-mathey <beatricemathey@gmail.com>
2022-11-21 14:56:50 -0300

    first commit

---
+++ src/componentes/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+export function Comment ({ content }){
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} 
+                    src='https://github.com/beatrice-mathey.png' alt='' />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                       <div className={styles.authorAndTime}>
+                            <strong>Beatrice Mathey</strong>
+                            <time title='21 de Outubro às 20h30min' dataTime='2022-10-21 20:30:00'>Cerca de 1h atrás </time>
+                       </div>
+                            <button title='Deletar comentário'>
+                                <Trash size={24} />
+                            </button>
+                    </header>
+
+                    
+                    <p>{content}</p>
+                </div>
+                
+            <footer>
+                <button>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </button>
+            </footer>
+            </div>
+        </div>
+    )
+}
